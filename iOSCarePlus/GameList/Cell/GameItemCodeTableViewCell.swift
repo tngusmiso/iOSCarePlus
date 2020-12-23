6478d7ff345ff0f0d582c2a3da16f0b85ba16665
tngusmiso <k1miso012@gmail.com>
2020-12-23 21:10:18 +0900

    [AutoLayout] GameItemCodeTableViewCell 코드로 오토레이아웃 작성하기 - 테스트용이므로 주석처리 함

--- iOSCarePlus/GameList/Cell/GameItemCodeTableViewCell.swift
+++ iOSCarePlus/GameList/Cell/GameItemCodeTableViewCell.swift
@@ -9,5 +9,45 @@
 import UIKit
 
 class GameItemCodeTableViewCell: UITableViewCell {
+    var gameImageView: UIImageView
+    var titleLabel: UILabel
+    var priceLabel: UILabel
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        gameImageView = UIImageView()
+        titleLabel = UILabel()
+        priceLabel = UILabel()
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(gameImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(priceLabel)
+        
+        gameImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gameImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            gameImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            gameImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            gameImageView.widthAnchor.constraint(equalToConstant: 122),
+            gameImageView.heightAnchor.constraint(equalToConstant: 69)
+        ])
+        
+        gameImageView.backgroundColor = .red
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: gameImageView.topAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: gameImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(greaterThanOrEqualTo: contentView.trailingAnchor, constant: 15)
+        ])
+        
+        titleLabel.text = "test"
+    }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- iOSCarePlus/GameList/GameListViewController.swift
+++ iOSCarePlus/GameList/GameListViewController.swift
@@ -19,6 +19,7 @@ class GameListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // tableView.register(GameItemCodeTableViewCell.self, forCellReuseIdentifier: "GameItemCodeTableViewCell")
         callNewGameListAPI()
     }
     
@@ -46,10 +47,11 @@ extension GameListViewController: UITableViewDataSource {
     
     // 해당 IndexPath(section, row)에 어떤 셀을 보여줄지!
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let content = model?.contents[indexPath.row] else { return UITableViewCell() }
-        let screenshotURLs: [String] = content.screenshots.reduce(into: []) { $0.append(contentsOf: $1.images.map { $0.url }) }
+        // let cell: GameItemCodeTableViewCell = tableView.dequeueReusableCell(withIdentifier: "GameItemCodeTableViewCell", for: indexPath) as! GameItemCodeTableViewCell
         
         let cell: GameItemTableViewCell = tableView.dequeueReusableCell(withIdentifier: "GameItemTableViewCell", for: indexPath) as! GameItemTableViewCell
+        guard let content = model?.contents[indexPath.row] else { return UITableViewCell() }
+        let screenshotURLs: [String] = content.screenshots.reduce(into: []) { $0.append(contentsOf: $1.images.map { $0.url }) }
         let model: GameItemModel = GameItemModel(gameTitle: content.formalName,
                                                  gameOriginPrice: 10_000,
                                                  gameDiscountPrice: nil,
