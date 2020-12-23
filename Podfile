ba2b91163fa3a282c850213be524560dc30c5681
tngusmiso <k1miso012@gmail.com>
2020-12-23 20:19:38 +0900

    [CocoaPod]  Kingfisher 설

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+
+target 'iOSCarePlus' do
+  pod 'Kingfisher', '~> 5.0'
+end
---
+++ iOSCarePlus/GameList/Cell/GameItemCodeTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GameItemCodeTableViewCell.swift
+//  iOSCarePlus
+//
+//  Created by 임수현 on 2020/12/23.
+//  Copyright © 2020 tngusmiso. All rights reserved.
+//
+
+import UIKit
+
+class GameItemCodeTableViewCell: UITableViewCell {
+
+}
